282760732767b20c8e9659d8f350d2f9a9d98b7a
Crystal <63870889+crys11@users.noreply.github.com>
2020-09-20 10:12:14 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log("JS Loaded!");
+
+const name = "Marc";
+
+console.log(name);
+
+// var hovering = function () {
+//   $("nav").show("slide", { direction: "right" }, 1000);
+// };
+
+// var leaving = function () {
+//   $("nav").hide("slide", { direction: "left" }, 1000);
+// };
+
+// $("#nav-container").hover(hovering, leaving);
